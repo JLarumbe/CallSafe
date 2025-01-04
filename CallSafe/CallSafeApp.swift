282ce463fa65afd92830a037a687e2af001794a5
JLarumbe <jorge.larumbem@gmail.com>
2025-01-04 14:13:36 -0500

    Initial Commit

---
+++ CallSafe/CallSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallSafeApp.swift
+//  CallSafe
+//
+//  Created by Jorge Larumbe on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CallSafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CallSafeTests/CallSafeTests.swift
@@ -0,0 +1,17 @@
+//
+//  CallSafeTests.swift
+//  CallSafeTests
+//
+//  Created by Jorge Larumbe on 1/4/25.
+//
+
+import Testing
+@testable import CallSafe
+
+struct CallSafeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
